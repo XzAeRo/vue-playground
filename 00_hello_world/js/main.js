86f696b63d4151d0a6fcb0354714e1735b426a65
Victor Gonzalez <victor.gonzalezro@gmail.com>
2017-03-07 17:39:53 -0300

    update latest examples
    Signed-off-by: Victor Gonzalez <victor.gonzalezro@gmail.com>

--- 00_hello_world/js/main.js
+++ 00_hello_world/js/main.js
@@ -11,3 +11,10 @@ var app2 = new Vue({
     message: 'You loaded this page on ' + new Date()
   }
 });
+
+var app3 = new Vue({
+  el: '#two-way-binding-app',
+  data: {
+    message: "You can change this text!"
+  }
+});
